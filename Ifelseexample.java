3e7f122d3cec3fdb9059c01b080a52f34d0e3c21
SANKETSKAPSE <128274630+SANKETSKAPSE@users.noreply.github.com>
2023-04-21 11:04:36 +0530

    done

---
+++ Ifelseexample.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Ifelseexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		
+		if(a!=10)
+			System.out.println("welcome");
+		else
+			System.out.println("Hello");
+	}
+
+}
